05ee20df25af8fbc49440de3a48287427727f657
Rishabh Nema <182422143+rishabhnema21@users.noreply.github.com>
2026-04-15 22:19:46 +0530

    update hero page ui

--- client/src/components/sections/home/Hero.tsx
+++ client/src/components/sections/home/Hero.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="h-screen pt-20">
-      <div className="px-4">
-        <div className="flex justify-between items-end">
-          <div className="font-[urbanist] px-4 py-4 text-6xl">
-            <h1 className="">Read more than words</h1>
-            <h1 className="italic font-light">Discover thoughts that stick.</h1>
+    <section className="w-screen pb-16">
+      <div className="px-1 md:px-4">
+        <div className="flex flex-col md:flex-row justify-between items-end">
+          <div className="font-[urbanist] w-full px-4 pt-9 pb-5 md:py-4 text-5xl md:text-6xl">
+            <h1 className="text-neutral-200">Read more than words</h1>
+            <h1 className="italic font-light text-neutral-200">Discover thoughts that stick.</h1>
           </div>
-          <div className="max-w-xs text-right text-sm  px-4 py-4 text-gray-800 font-light">
+          <div className="max-w-xs text-right text-sm  px-4 py-1 md:py-4 text-gray-400 font-light">
             <p>
               A quiet place where stories grow beyond the page, where every
               thought you gather shapes your own unique journey through words
@@ -19,20 +19,24 @@ const Hero = () => {
           </div>
         </div>
         <div className="px-4 pt-3 flex gap-5 items-end">
-          <Image
+          <div className="h-100 w-1/2">
+            <Image
             src="/heroBg.jpg"
             alt="image"
             width={595}
             height={20}
-            className="rounded-md contrast-125"
+            className="rounded-md contrast-125 h-full w-full"
           />
-          <Image
-            src="/image.jfif"
+          </div>
+          <div className="h-100 w-1/2">
+            <Image
+            src="/heroImage.jpg"
             alt="image"
             width={750}
             height={20}
-            className="rounded-md"
+            className="rounded-md hidden md:block w-full h-full object-cover"
           />
+          </div>
         </div>
       </div>
     </section>
