d09f935c47f8d5869b4c778c1e34788a33d9268e
Rishabh Nema <182422143+rishabhnema21@users.noreply.github.com>
2026-07-03 21:27:39 +0530

    fix: update CORS origin to use environment variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import uploadRoutes from "./routes/upload.route";
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
 }))
 
